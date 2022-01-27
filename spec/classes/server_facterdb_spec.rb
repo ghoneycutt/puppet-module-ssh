b5f49804a378a189a7a27a398dfbd06927771b2c
Phil Friderici <phil.friderici@elits.com>
2022-01-27 15:48:53 +0000

    Add support for 'include' keyword to ssh::server class
    - used by Ubuntu 20.04

--- spec/classes/server_facterdb_spec.rb
+++ spec/classes/server_facterdb_spec.rb
@@ -223,6 +223,7 @@ describe 'ssh::server' do
       'host_key_algorithms'                  => { str: 'HostKeyAlgorithms',                val: [['ssh-ed25519'], ['ssh-ed25519', 'rsa-sha2-512']], sep: ',', },
       'ignore_rhosts'                        => { str: 'IgnoreRhosts',                     val: ['yes', 'no'], },
       'ignore_user_known_hosts'              => { str: 'IgnoreUserKnownHosts',             val: ['yes', 'no'], },
+      'include'                              => { str: 'Include',                          val: ['/test/ing', '~/test/ing'], },
       'ip_qos'                               => { str: 'IPQoS',                            val: ['af42', 'af42 cs3'], },
       'kbd_interactive_authentication'       => { str: 'KbdInteractiveAuthentication',     val: ['yes', 'no'], },
       'kerberos_authentication'              => { str: 'KerberosAuthentication',           val: ['yes', 'no'], },
