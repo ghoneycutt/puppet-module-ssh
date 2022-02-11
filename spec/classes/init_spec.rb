2798147d37171c3d891f9420e12b09106b318d93
Phil Friderici <phil.friderici@elits.com>
2022-02-11 10:58:15 +0000

    Refactor unit tests for main class
    - move OS specific facts to global position
    - run OS independent only on RedHat 7

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2977,62 +2977,78 @@ describe 'sshd_config_print_last_log param' do
 >>>>>>> 88c28a8 (Remove export/import functionality for SSH keys)
 =======
   on_supported_os.sort.each do |os, os_facts|
-    context "on #{os} with default values for parameters" do
-      # OS specific SSH versions
-      case "#{os_facts[:os]['name']}-#{os_facts[:os]['release']['major']}"
-      when 'CentOS-5', 'OracleLinux-5', 'RedHat-5', 'Scientific-5'
-        ssh_version = 'OpenSSH_4.3p2'
-        ssh_version_numeric = '4.3'
-      when 'CentOS-6', 'OracleLinux-6', 'RedHat-6', 'Scientific-6'
-        ssh_version = 'OpenSSH_5.3p1'
-        ssh_version_numeric = '5.3'
-      when 'CentOS-7', 'OracleLinux-7', 'RedHat-7', 'Scientific-7'
-        ssh_version = 'OpenSSH_6.6p1'
-        ssh_version_numeric = '6.6'
-      when 'SLED-10', 'SLES-10'
-        ssh_version = 'OpenSSH_5.1p1'
-        ssh_version_numeric = '5.1'
-      when 'SLED-11', 'SLED-12', 'SLES-11', 'SLES-12'
-        ssh_version = 'OpenSSH_6.6.1p1'
-        ssh_version_numeric = '6.6'
-      when 'SLED-15', 'SLES-15'
-        ssh_version = 'OpenSSH_8.4p1'
-        ssh_version_numeric = '8.4'
-      when 'Solaris-9', 'Solaris-10', 'Solaris-11'
-        ssh_version = 'Sun_SSH_2.2'
-        ssh_version_numeric = '2.2'
-      when 'Ubuntu-14.04'
-        ssh_version = 'OpenSSH 6.6p1'
-        ssh_version_numeric = '6.6'
-      when 'Ubuntu-16.04'
-        ssh_version = 'OpenSSH_7.2p2'
-        ssh_version_numeric = '7.2'
-      when 'Ubuntu-18.04'
-        ssh_version = 'OpenSSH_7.6p1'
-        ssh_version_numeric = '7.6'
-      when 'Ubuntu-20.04'
-        ssh_version = 'OpenSSH_8.2p1'
-        ssh_version_numeric = '8.2'
-      when 'Debian-7'
-        ssh_version = 'OpenSSH_6.0p1'
-        ssh_version_numeric = '6.0'
-      when 'Debian-8'
-        ssh_version = 'OpenSSH_6.7p1'
-        ssh_version_numeric = '6.7'
-      when 'Debian-9'
-        ssh_version = 'OpenSSH_7.4p1'
-        ssh_version_numeric = '7.4'
-      when 'Debian-10'
-        ssh_version = 'OpenSSH_7.9p1'
-        ssh_version_numeric = '7.9'
-      when 'Debian-11'
-        ssh_version = 'OpenSSH_8.4p1'
-        ssh_version_numeric = '8.4'
-      else
-        ssh_version = 'UnkownSSH_2.42'
-        ssh_version_numeric = '2.42'
-      end
+    # OS specific SSH version facts
+    case "#{os_facts[:os]['name']}-#{os_facts[:os]['release']['major']}"
+    when 'CentOS-5', 'OracleLinux-5', 'RedHat-5', 'Scientific-5'
+      ssh_version = 'OpenSSH_4.3p2'
+      ssh_version_numeric = '4.3'
+    when 'CentOS-6', 'OracleLinux-6', 'RedHat-6', 'Scientific-6'
+      ssh_version = 'OpenSSH_5.3p1'
+      ssh_version_numeric = '5.3'
+    when 'CentOS-7', 'OracleLinux-7', 'RedHat-7', 'Scientific-7'
+      ssh_version = 'OpenSSH_6.6p1'
+      ssh_version_numeric = '6.6'
+    when 'SLED-10', 'SLES-10'
+      ssh_version = 'OpenSSH_5.1p1'
+      ssh_version_numeric = '5.1'
+    when 'SLED-11', 'SLED-12', 'SLES-11', 'SLES-12'
+      ssh_version = 'OpenSSH_6.6.1p1'
+      ssh_version_numeric = '6.6'
+    when 'SLED-15', 'SLES-15'
+      ssh_version = 'OpenSSH_8.4p1'
+      ssh_version_numeric = '8.4'
+    when 'Solaris-9', 'Solaris-10', 'Solaris-11'
+      ssh_version = 'Sun_SSH_2.2'
+      ssh_version_numeric = '2.2'
+    when 'Ubuntu-14.04'
+      ssh_version = 'OpenSSH 6.6p1'
+      ssh_version_numeric = '6.6'
+    when 'Ubuntu-16.04'
+      ssh_version = 'OpenSSH_7.2p2'
+      ssh_version_numeric = '7.2'
+    when 'Ubuntu-18.04'
+      ssh_version = 'OpenSSH_7.6p1'
+      ssh_version_numeric = '7.6'
+    when 'Ubuntu-20.04'
+      ssh_version = 'OpenSSH_8.2p1'
+      ssh_version_numeric = '8.2'
+    when 'Debian-7'
+      ssh_version = 'OpenSSH_6.0p1'
+      ssh_version_numeric = '6.0'
+    when 'Debian-8'
+      ssh_version = 'OpenSSH_6.7p1'
+      ssh_version_numeric = '6.7'
+    when 'Debian-9'
+      ssh_version = 'OpenSSH_7.4p1'
+      ssh_version_numeric = '7.4'
+    when 'Debian-10'
+      ssh_version = 'OpenSSH_7.9p1'
+      ssh_version_numeric = '7.9'
+    when 'Debian-11'
+      ssh_version = 'OpenSSH_8.4p1'
+      ssh_version_numeric = '8.4'
+    else
+      ssh_version = 'UnkownSSH_2.42'
+      ssh_version_numeric = '2.42'
+    end
 
+    # OS specific module defaults
+    case "#{os_facts[:os]['name']}-#{os_facts[:os]['release']['full']}"
+    when %r{CentOS.*}, %r{OracleLinux.*}, %r{RedHat.*}, %r{Scientific.*}
+      packages_default = ['openssh-clients']
+    when %r{SLED.*}, %r{SLES.*}
+      packages_default = ['openssh']
+    when %r{Debian.*}, %r{Ubuntu.*}
+      packages_default = ['openssh-client']
+    when %r{Solaris-9.*}, %r{Solaris-10.*}
+      packages_default    = ['SUNWsshcu', 'SUNWsshr', 'SUNWsshu']
+      packages_ssh_source = '/var/spool/pkg'
+    when %r{Solaris-11.*}
+      packages_default    = ['network/ssh', 'network/ssh/ssh-key']
+      packages_ssh_source = nil
+    end
+
+    describe "on #{os} with default values for parameters" do
       let(:facts) do
         os_facts.merge(
           {
@@ -3041,22 +3057,6 @@ describe 'sshd_config_print_last_log param' do
             ssh_version_numeric: ssh_version_numeric,
           },
         )
-      end
-
-      # OS specific defaults
-      case "#{os_facts[:os]['name']}-#{os_facts[:os]['release']['full']}"
-      when %r{CentOS.*}, %r{OracleLinux.*}, %r{RedHat.*}, %r{Scientific.*}
-        packages_default = ['openssh-clients']
-      when %r{SLED.*}, %r{SLES.*}
-        packages_default = ['openssh']
-      when %r{Debian.*}, %r{Ubuntu.*}
-        packages_default = ['openssh-client']
-      when %r{Solaris-9.*}, %r{Solaris-10.*}
-        packages_default    = ['SUNWsshcu', 'SUNWsshr', 'SUNWsshu']
-        packages_ssh_source = '/var/spool/pkg'
-      when %r{Solaris-11.*}
-        packages_default    = ['network/ssh', 'network/ssh/ssh-key']
-        packages_ssh_source = nil
       end
 
       it { is_expected.to compile.with_all_deps }
@@ -3113,6 +3113,20 @@ describe 'sshd_config_print_last_log param' do
       it { is_expected.to have_ssh_authorized_key_resource_count(0) }
       it { is_expected.to contain_class('ssh::server') }
     end
+  end
+
+  # The following tests are OS independent, so we only test one
+  redhat = {
+    supported_os: [
+      {
+        'operatingsystem'        => 'RedHat',
+        'operatingsystemrelease' => ['7'],
+      },
+    ],
+  }
+
+  on_supported_os(redhat).sort.each do |os, os_facts|
+    let(:facts) { os_facts.merge({ root_home: '/root' }) }
 
     context "on #{os} with config_entries set to valid hash" do
       let(:params) do
@@ -3271,7 +3285,6 @@ describe 'sshd_config_print_last_log param' do
     end
 
     context "on #{os} with manage_root_ssh_config set to valid true" do
-      let(:facts) { os_facts.merge({ root_home: '/root' }) }
       let(:params) { { manage_root_ssh_config: true } }
 
       it do
@@ -3318,57 +3331,23 @@ describe 'sshd_config_print_last_log param' do
     end
 
     context "on #{os} with package_adminfile set to valid /unit/test" do
-      let(:facts) { os_facts  }
       let(:params) { { package_adminfile: '/unit/test' } }
 
-      # OS specific defaults
-      case "#{os_facts[:os]['name']}-#{os_facts[:os]['release']['full']}"
-      when %r{CentOS.*}, %r{OracleLinux.*}, %r{RedHat.*}, %r{Scientific.*}
-        packages_default = ['openssh-clients']
-      when %r{SLED.*}, %r{SLES.*}
-        packages_default = ['openssh']
-      when %r{Debian.*}, %r{Ubuntu.*}
-        packages_default = ['openssh-client']
-      when %r{Solaris-9.*}, %r{Solaris-10.*}
-        packages_default    = ['SUNWsshcu', 'SUNWsshr', 'SUNWsshu']
-      when %r{Solaris-11.*}
-        packages_default    = ['network/ssh', 'network/ssh/ssh-key']
-      end
-
-      packages_default.each do |package|
-        it { is_expected.to contain_package(package).with_adminfile('/unit/test') }
-      end
+      it { is_expected.to contain_package('openssh-clients').with_adminfile('/unit/test') }
     end
 
     context "on #{os} with packages set to valid array [unit, test]" do
       let(:params) { { packages: ['unit', 'test'] } }
 
-      it { is_expected.to have_package_resource_count(2) }
+      it { is_expected.to have_package_resource_count(3) } # [unit, test] + [openssh-server] from ssh::server
       it { is_expected.to contain_package('unit') }
       it { is_expected.to contain_package('test') }
     end
 
     context "on #{os} with package_source set to valid /unit/test" do
-      let(:facts) { os_facts }
       let(:params) { { package_source: '/unit/test' } }
 
-      # OS specific defaults
-      case "#{os_facts[:os]['name']}-#{os_facts[:os]['release']['full']}"
-      when %r{CentOS.*}, %r{OracleLinux.*}, %r{RedHat.*}, %r{Scientific.*}
-        packages_default = ['openssh-clients']
-      when %r{SLED.*}, %r{SLES.*}
-        packages_default = ['openssh']
-      when %r{Debian.*}, %r{Ubuntu.*}
-        packages_default = ['openssh-client']
-      when %r{Solaris-9.*}, %r{Solaris-10.*}
-        packages_default    = ['SUNWsshcu', 'SUNWsshr', 'SUNWsshu']
-      when %r{Solaris-11.*}
-        packages_default    = ['network/ssh', 'network/ssh/ssh-key']
-      end
-
-      packages_default.each do |package|
-        it { is_expected.to contain_package(package).with_source('/unit/test') }
-      end
+      it { is_expected.to contain_package('openssh-clients').with_source('/unit/test') }
     end
 
     context "on #{os} with purge_keys set to valid false" do
@@ -3378,7 +3357,6 @@ describe 'sshd_config_print_last_log param' do
     end
 
     context "on #{os} with root_ssh_config_content set to valid #unit test (when manage_root_ssh_config is true)" do
-      let(:facts) { os_facts.merge({ root_home: '/root' }) }
       let(:params) do
         {
           root_ssh_config_content: '#unit test',
