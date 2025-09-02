6c222d8a3a640f974837b81ff6c2bc66ffd5337b
Michael Prokop <mprokop@sipwise.com>
2025-09-02 08:02:49 +0200

    Add support for Debian 13
    Debian 13 (trixie) is the latest new stable release.
    
    Signed-off-by: Michael Prokop <mprokop@sipwise.com>
    Co-Authored-By: Garrett Honeycutt <code@garretthoneycutt.com>

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -41,7 +41,7 @@ describe 'ssh::server' do
         packages          = []
         service_name      = 'sshd'
         service_hasstatus = true
-      when %r{Ubuntu-(22.04|24.04|26.04)}
+      when %r{Debian-13}, %r{Ubuntu-(22.04|24.04|26.04)}
         config_mode       = '0600'
         packages          = ['openssh-server']
         service_hasstatus = true
